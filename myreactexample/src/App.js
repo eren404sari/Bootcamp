0f36e8391100258d758d541edbce6bb3430c7747
Erin Sari <esari99@gmail.com>
2023-04-25 10:53:23 -0400

    reorganization

--- myreactexample/src/App.js
+++ myreactexample/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Login from "./components/login.js";
-import Signup from "./components/signup.js";
+import Login from "./Utils/loginUtil.js";
+import Signup from "./Utils/signupUtil.js";
 import Home from "./components/home.js";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -20,13 +20,15 @@ axios.interceptors.request.use(
   }
 );
 
-
+//for now the default path is to login immediately
+//future implementation to keep user signed in
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/sign up" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
