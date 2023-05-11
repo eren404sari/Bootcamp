c5f1e08743678ab169ed800a80b293ada4e4f9b4
Erin Sari <esari99@gmail.com>
2023-05-11 11:13:14 -0400

    begining login on express example

---
+++ 5-11 expressexample/routes/auth.js
@@ -0,0 +1,85 @@
+const User = require("../model/user")
+var express = require('express');
+const brcypt = require("bcryptjs")
+var router = express.Router();
+const jwt = require("jsonwebtoken")
+
+//here we have tio do the following :- 
+// 1. Get the user input 
+// 2. Validate the input 
+// 3. Validate if the user the same email already exists the input 
+// 4. using Brcypt we will encrypt the password 
+// 5. create a user in mongodb database 
+// finally we need to create a signed JWT token that shall be expired after 24 hours 
+
+
+/* GET home page. */
+router.post('/register', async (req, res, next) => {
+    try {
+        const { firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword } = req.body;
+
+
+
+        if (!(email && password && confirmPassword && email, firstName, lastName)) {
+            res.status(400).send({ error: "All Fields are required" });
+        }
+
+        if (password !== confirmPassword) {
+            res.status(400).send({ error: `The password ${password} and confirm password ${confirmPassword} does not macth !` });
+        }
+
+        const existingUser = await User.findOne({ email })
+
+        console.log("EXIST   " + existingUser)
+        if (existingUser) {
+            return res.status(400).send({ error: `The user with email : ${email} already exists` });
+        }
+
+        // encrypt the password 
+
+        const enrcyptedPassword = await brcypt.hash(password, 10);
+        const user = await User.create({
+            firstName,
+            lastName,
+            email: email.toLowerCase(),
+            password: enrcyptedPassword
+        });
+
+        // creating a token with some expiry time 
+
+        const token = jwt.sign({
+            id: user._id, email
+        }, process.env.TOKEN_KEY, { expiresIn: "2h" });
+        user.token = token
+
+        res.status(201).json(user)
+
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+router.post('/login', function (req, res, next) {
+    // implementation goes here 
+    // 1. get the user input from req.body {email, password}
+    res.send(`Email: ${email}`);
+    res.send(`Password: ${password}`);
+
+    // 2. validate it see validations done in register
+    
+
+    // 3. validate from db if the user exist if yes then proceed otherwise send a json resone stating error look at register
+    //const user = user.find(
+    //    (usr) => usr.getEmail().toLowerCase() === email.toLowerCase(),
+    //   (usr) => usr.getPassword() === password
+    //)
+
+    // 4. if the user with email exist in database then make a token assign the expiratory time 2hrs and respond with token generated
+
+    
+});
+module.exports = router;
